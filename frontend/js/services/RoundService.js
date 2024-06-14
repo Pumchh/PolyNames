f8a41245e72e449be0cdec97e342232341ee84fb
Pumchh <47301060+Pumchh@users.noreply.github.com>
2024-06-14 21:29:36 +0200

    Add round service

---
+++ frontend/js/services/RoundService.js
@@ -0,0 +1,24 @@
+export class RoundService {
+
+    static async createRound() {
+        const response = await fetch("http://localhost:8080/create_round")
+        if(response.status === 200){
+            console.log("Round Created")
+        }
+    }
+    
+    
+    static async getRoundID() {
+        const response = await fetch("http://localhost:8080/get_roundID")
+        if(response.status === 200){
+            let data = await response.json()
+            //console.log(data);
+            return data;
+        }
+    }
+
+    static async setScore(){
+        const response = await fetch("http://localhost:8080/")
+    }
+
+}
